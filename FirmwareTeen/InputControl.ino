5cc2f3a3b0c2b7dcb311a31129de39acceee6985
tfleig <tony.fleig@gmail.com>
2020-12-05 16:52:54 -0800

    Removed debugging code inadvertantly included in my last commit.

--- FirmwareTeen/InputControl.ino
+++ FirmwareTeen/InputControl.ino
@@ -224,25 +224,6 @@ int invdrop[12][4] = {
   { 0, 12, 12, 0  }   // 3rd inv drop 3
 };
 
-
-#ifdef __arm__
-// should use uinstd.h to define sbrk but Due causes a conflict
-extern "C" char* sbrk(int incr);
-#else  // __ARM__
-extern char *__brkval;
-#endif  // __arm__
-
-int freeMemory() {
-  char top;
-#ifdef __arm__
-  return &top - reinterpret_cast<char*>(sbrk(0));
-#elif defined(CORE_TEENSY) || (ARDUINO > 103 && ARDUINO != 151)
-  return &top - __brkval;
-#else  // __arm__
-  return __brkval ? &top - __brkval : &top - __malloc_heap_start;
-#endif  // __arm__
-}
-
 /**
     \brief Provides functionality when inputs or gate change
 
